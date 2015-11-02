5881942b933bcb562e134eacab823656f6f8cfd3
sheck <sheck@list.ru>
2015-11-02 21:00:04 +0000

    Whoops

--- src/app.php
+++ src/app.php
@@ -2,4 +2,24 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+* Register the error handler
+*/
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+//throw new \Exception;
+
+
 echo 'Init';
